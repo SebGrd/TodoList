117fa8aa4f128354a35936d19ee37f1613d38db6
3kezoh <3kezoh@gmail.com>
2021-12-29 00:01:48 +0100

    ✨ Add timestamps in User

--- src/models/user.ts
+++ src/models/user.ts
@@ -9,32 +9,35 @@ export interface UserDocument extends Document {
   birthdate: Date;
 }
 
-const UserSchema = new Schema<UserDocument>({
-  email: {
-    type: String,
-    required: true,
-  },
-  firstname: {
-    type: String,
-    required: [true, "Firstname should be at least 1 character long."],
-  },
-  lastname: {
-    type: String,
-    required: [true, "Lastname should be at least 1 character long."],
-  },
-  password: {
-    type: String,
-    minlength: [8, "Password should be at least 8 characters long."],
-    maxlength: [40, "Password should not exceed 40 characters."],
-  },
-  birthdate: {
-    type: Date,
-    required: true,
-    validate: {
-      validator: (birthdate: Date) => dayjs().diff(birthdate, "year") >= 13,
+const UserSchema = new Schema<UserDocument>(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: [true, "Firstname should be at least 1 character long."],
+    },
+    lastname: {
+      type: String,
+      required: [true, "Lastname should be at least 1 character long."],
+    },
+    password: {
+      type: String,
+      minlength: [8, "Password should be at least 8 characters long."],
+      maxlength: [40, "Password should not exceed 40 characters."],
+    },
+    birthdate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (birthdate: Date) => dayjs().diff(birthdate, "year") >= 13,
+      },
     },
   },
-});
+  { timestamps: true }
+);
 
 const User = model<UserDocument>("User", UserSchema);
 
